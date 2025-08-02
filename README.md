05cc163bed5b64483771c34d434f2f192589eda5
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-08-03 01:17:31 +0530

    Project setup and routes

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 - create a Vite + react.
 - remove unnecessary code.
 - Install Tailwind css
+
+
+Body 
+    navBar
+    Route=/ - Feed
+    Route=/login - login
+    Route=/connections - Connections
+    Route=/profile - profile.
---
+++ src/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar';
+import Footer from './Footer';
+import { Outlet } from 'react-router';
+
+function Body() {
+
+  return (
+    <>
+    <NavBar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Body
